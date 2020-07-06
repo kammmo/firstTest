651ea520b21e8e2f028ac2f80708c408e9a3e395
kammmo <kammmo@pisaci-stroj>
2020-07-06 20:08:48 +0200

    Initial commit

---
+++ firstGitt/src/firstgitt/FirstGitt.java
@@ -0,0 +1,34 @@
+/* The file is saved in UTF-8 codepage.
+ * Check: «Stereotype», Section mark-§, Copyright-©, Alpha-α, Beta-β, Smile-☺
+ */
+package firstgitt;
+
+
+
+/*******************************************************************************
+ * The class {@code FirstGitt} is the main class of the project
+ * that ...
+ *
+ * @author  Rudolf PECINOVSKÝ
+ * @version 0.00.0000 — 20yy-mm-dd
+ */
+public class FirstGitt
+{
+    /***************************************************************************
+     * Method starting the application.
+     * Hlavní metoda apl ikace
+     *
+     * @param args Command line arguments - not used here
+     * @param args Parametry příkazového řádku - zde nepoužité
+     */
+    public static void main(String[] args)
+    {
+        //TODO Starting code
+        //TODO Tělo hlavní metody
+    }
+
+
+    /** Private constructor preventing instance creation.*/
+    /** Soukromý konstruktor zabraňující vytvoření instance. */
+    private FirstGitt() {}
+}
